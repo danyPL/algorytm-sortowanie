145c49ca91eae0da32951a8b82affbb854861d84
danyPL <renatasiwiec71@gmail.com>
2024-04-22 10:07:19 +0200

    Dodaj pliki projektów.

---
+++ algorytm-sortowanie/Program.cs
@@ -0,0 +1,54 @@
+﻿using algorytm_sortowanie;
+
+List<int> cyfry = new List<int>();
+bool repeat = true;
+Console.WriteLine("Podaj cyfry do arrayu np. 1,2,3,4");
+string[] cyfryS = Console.ReadLine().Split(",");
+foreach (string cyfra in cyfryS)
+    cyfry.Add(int.Parse(cyfra));
+int[] temp;
+
+Sorting sort = new Sorting(cyfry.ToArray());
+while (repeat)
+{
+    Console.Clear();
+ Console.WriteLine("1. Znajdz najmniejsza");
+    Console.WriteLine("2. Znajdz najwieksza");
+    Console.WriteLine("3. Najmniejsza i podmien");
+    Console.WriteLine("4. Posortuj");
+    Console.WriteLine("5. Wyjdz");
+
+    string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine(sort.FindLowest());
+            Console.ReadKey();
+            break;
+        case "2":
+            Console.WriteLine(sort.FindHighest());
+            Console.ReadKey();
+
+            break;
+        case "3":
+        temp = sort.FindLowestAndExchange();
+            foreach (int cyfra in temp)
+                Console.Write($"{cyfra}, ");
+
+            Console.ReadKey();
+
+            break;
+        case "4":
+        temp = sort.Sort(cyfry.ToArray());
+            foreach (int cyfra in temp)
+                Console.Write($"{cyfra}, ");
+
+            Console.ReadKey();
+
+            break;
+        case "5":
+            repeat = false;
+            break;
+    }
+}
+   
---
+++ algorytm-sortowanie/Sorting.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorytm_sortowanie
+{
+    public class Sorting
+    {
+        public int[] cyfry; 
+        public Sorting(int[] cyfry)
+        {
+            this.cyfry = cyfry;
+        }
+        /**************************************
+     * nazwa funkcji:       FindHighest()
+     * parametry wejściowe: Bierze wartosci z int[] cyfry.
+     * wartość zwracana:    int, największa liczba w arrayu
+     * autor:               Daniel Siwiec
+     *************************************/
+        public int FindHighest()
+        {
+            int currentHigh = int.MinValue;
+
+            foreach(int cyfra in cyfry)
+            {
+                if (cyfra > currentHigh)
+                {
+                    currentHigh = cyfra;
+                }
+            }
+            return currentHigh;
+        }
+        /**************************************
+  * nazwa funkcji:       FindLowest()
+  * parametry wejściowe: Bierze wartosci z int[] cyfry.
+  * wartość zwracana:    int, najmniejsza liczba w arrayu
+  * autor:               Daniel Siwiec
+  *************************************/
+        public int FindLowest()
+        {
+            int currentLow = int.MaxValue;
+            foreach(int cyfra in cyfry)
+            {
+                if(cyfra < currentLow)
+                {
+                    currentLow = cyfra;
+                }
+            }
+            return currentLow;
+        }
+        /**************************************
+ * nazwa funkcji:       FindLowestAndExchange()
+ * parametry wejściowe: Bierze wartosci z int[] _cyfryC.
+ * wartość zwracana:    int, najmniejsza liczba w arrayu
+ * autor:               Daniel Siwiec
+ *************************************/
+        public int[] FindLowestAndExchange()
+        {
+            int[] _cyfryC = (int[])cyfry.Clone();
+            int lowestValue = FindLowest();
+
+            for (int i = 0; i < _cyfryC.Length; i++)
+            {
+                if (_cyfryC[i] == lowestValue)
+                    _cyfryC[i] = i;
+            }
+            return _cyfryC;
+        }
+        /**************************************
+* nazwa funkcji:       Sort()
+* parametry wejściowe: Bierze wartosci z int[] sorted_array
+* wartość zwracana:    Posortowana tablica metodą przez wybieranie
+* autor:               Daniel Siwiec
+*************************************/
+        public int[] Sort(int[] sorted_array)
+        {
+            for(int i = 0; i < sorted_array.Length - 1; i++)
+            {
+                int min = i;
+              for(int j=i + 1; j < sorted_array.Length; j++)
+                {
+                    if (sorted_array[min] > sorted_array[j])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = sorted_array[i];
+                sorted_array[i] = sorted_array[min];
+                sorted_array[min] = temp;
+            }
+
+            return sorted_array;
+        }
+
+    }
+}
